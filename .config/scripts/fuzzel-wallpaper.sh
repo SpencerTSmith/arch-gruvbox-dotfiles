6462b73af250f7e8612b9d2ab83f72b63a04a93c
SpencerTSmith <spencertsmith2003@gmail.com>
2025-06-01 19:43:54 -0500

    fix fuzzel dmenu wallpaper script

--- .config/scripts/fuzzel-wallpaper.sh
+++ .config/scripts/fuzzel-wallpaper.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 WALLPAPER_DIR=$HOME/.config/wallpapers
+WALLPAPER_DIR_WRITE=\$HOME/.config/wallpapers
 WALLPAPERS=$(find $WALLPAPER_DIR -type f -printf "%f\n")
 
 HYPRPAPER_DIR=$HOME/.config/hypr/
@@ -16,5 +17,5 @@ CHOICE=$(printf '%s\n' "$WALLPAPERS" \
 
 if [[ -n "$CHOICE" ]]; then
   hyprctl hyprpaper reload , "$WALLPAPER_DIR/$CHOICE"
-  echo -e "preload = $WALLPAPER_DIR/$CHOICE\nwallpaper = , $WALLPAPER_DIR/$CHOICE" > $HYPRPAPER_DIR/hyprpaper.conf
+  echo -e "preload = $WALLPAPER_DIR_WRITE/$CHOICE\nwallpaper = , $WALLPAPER_DIR_WRITE/$CHOICE" > $HYPRPAPER_DIR/hyprpaper.conf
 fi
