81b7b97436aa5ee4a31c5beff600b8d964a81a41
Spencer Smith <spencertsmith2003@gmail.com>
2025-11-03 19:24:18 -0600

    warmth

--- .local/bin/hyprsunset_toggle.sh
+++ .local/bin/hyprsunset_toggle.sh
@@ -4,5 +4,5 @@ FLAG="$XDG_STATE_HOME/hyprsunset_on"
 if [ -f "$FLAG" ]; then
   hyprctl hyprsunset identity && rm "$FLAG"
 else
-  hyprctl hyprsunset temperature 3000 && touch "$FLAG"
+  hyprctl hyprsunset temperature 2500 && touch "$FLAG"
 fi
